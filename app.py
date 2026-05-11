3416e91586361ca5104af4a9d39623b58c9d8fbd
Heryyy1990 <v4ndalis90@gmail.com>
2026-05-11 20:01:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1869,7 +1869,7 @@ def halaman_utama():
                 display: inline-block !important; 
             }
             details[open] > summary .arrow-icon {
-                transform: rotate(270deg) !important; 
+                transform: rotate(180deg) !important; 
             }
 
             </style>
