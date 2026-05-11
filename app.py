545f31eb7d923807a62f24ca5b744d92546233a3
Heryyy1990 <v4ndalis90@gmail.com>
2026-05-11 20:47:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1366,7 +1366,7 @@ def halaman_utama():
                 width: 100% !important; max-width: 580px !important; 
                 margin-left: auto !important; margin-right: auto !important;
                 position: relative; z-index: 99;
-                padding-left: 50px !important;
+                padding-left: 30px !important;
             }
             
             /* Desain Input Streamlit */
