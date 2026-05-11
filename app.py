937731a392bb96b15daf45d6d2e92467517d9b8c
Heryyy1990 <v4ndalis90@gmail.com>
2026-05-12 00:13:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -591,11 +591,11 @@ input[type="text"], input[type="password"] {
     background-size: 20px !important;
 }
 
-input[aria-label="Username"] {
+div[data-testid="stTextInput"]:nth-of-type(1) input {
     background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="%23009DFF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>') !important;
 }
 
-input[aria-label="Password"] {
+div[data-testid="stTextInput"]:nth-of-type(2) input {
     background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="%23009DFF" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>') !important;
 }
 
